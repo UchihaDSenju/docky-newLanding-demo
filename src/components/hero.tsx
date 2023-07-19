dc6de4222d414952c8a04a61a69fe35b9b5c5c95
Tariq <rafiqtariq19@gmail.com>
2023-07-19 19:20:45 +0530

    hero section designed:

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,12 @@ function Hero() {
     <>
         <div className="grid grid-cols-2">
             <div><img src={Doctor} alt="man with stethoscope" /></div>
-            <div>Content 2</div>
+            <div className="flex flex-col justify-center">
+              <h1 className="text-5xl text-[#1D4589] font-bold">Effortless Management</h1>
+              <h1 className="text-5xl text-[#22B571] font-bold">For Exceptional Healthcare Experiences</h1>
+              <br />
+              <p>DOCKY can help you streamline and automate various aspects of healthcare administration — right from patient registration, database management, lab reporting, electronic health records, clinic management and more.</p>
+            </div>
         </div>
     </>
   )
