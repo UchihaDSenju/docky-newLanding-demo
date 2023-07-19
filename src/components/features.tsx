23eba4a79b068cef0b198533791bf1b6111379a8
Tariq <rafiqtariq19@gmail.com>
2023-07-19 19:38:27 +0530

    features design over

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -3,13 +3,14 @@ import FeatureCard from "./feature-card"
 function Features() {
   return (
     <>
-        <div className="text-center text-lg">
-            <h1>Every Thing You Need</h1> 
-            <div className="flex justify-evenly">
+        <div className="text-center text-lg  bg-blue-200 p-5 rounded-3xl">
+            <h1 className="text-4xl font-bold text-blue-400 my-5">Every Thing You Need</h1> 
+            <div className="flex justify-evenly my-5">
                 <FeatureCard desc="Card-1" />
                 <FeatureCard desc="Card-1" />
                 <FeatureCard desc="Card-1" />
             </div>
+            <p className="my-2">Looking fore more? <a href="#" className="underline-offset-1">Please get in touch</a></p>
         </div>
     </>
   )
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,13 @@
-
 import Doctor from '../assets/man-with-stethoscope-his-shirt-is-standing-front-brown-door copy 1.png'
-
+import '../styles/demo.css'
 function Hero() {
   return (
     <>
         <div className="grid grid-cols-2">
             <div><img src={Doctor} alt="man with stethoscope" /></div>
             <div className="flex flex-col justify-center">
-              <h1 className="text-5xl text-[#1D4589] font-bold">Effortless Management</h1>
-              <h1 className="text-5xl text-[#22B571] font-bold">For Exceptional Healthcare Experiences</h1>
+              <h1 className="text-5xl blue-text font-bold">Effortless Management</h1>
+              <h1 className="text-5xl text-green-400 font-bold">For Exceptional Healthcare Experiences</h1>
               <br />
               <p>DOCKY can help you streamline and automate various aspects of healthcare administration — right from patient registration, database management, lab reporting, electronic health records, clinic management and more.</p>
             </div>
